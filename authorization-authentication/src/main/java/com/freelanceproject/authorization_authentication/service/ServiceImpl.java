46ddef1d5b2ca3d73e4fecff0af43aa120c4e391
“hssinee” <ahmed.hassine@esprit.tn>
2025-03-20 04:52:45 +0100

    update

--- authorization-authentication/src/main/java/com/freelanceproject/authorization_authentication/service/ServiceImpl.java
+++ authorization-authentication/src/main/java/com/freelanceproject/authorization_authentication/service/ServiceImpl.java
@@ -40,7 +40,7 @@ public class ServiceImpl {
             return "Username already taken";
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole("ROLE_USER");
+        user.setRole("ROLE_" + user.getRole());
         userRepository.save(user);
 
         return "User Registered successfully";
